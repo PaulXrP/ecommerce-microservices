1167bc204ff884869717478ac055dccf786a71eb
PaulXrP <pranaypaul@icloud.com>
2025-04-17 20:39:42 +0530

    Added clients for inter-service communication in order service..

---
+++ order-service/src/main/java/com/pranay/ecommerce/order_service/clients/ProductServiceClientConfig.java
@@ -0,0 +1,30 @@
+package com.pranay.ecommerce.order_service.clients;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.Optional;
+
+@Configuration
+public class ProductServiceClientConfig {
+
+
+    public ProductServiceClient productServiceInterface(RestClient.Builder restClientBuilder) {
+         RestClient restClient = restClientBuilder
+                 .baseUrl("http://product-service")
+                 .defaultStatusHandler(HttpStatusCode::is4xxClientError,
+                         ((request, response) -> Optional.empty()))
+                 .build();
+
+        RestClientAdapter restClientAdapter = RestClientAdapter.create(restClient);
+        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory
+                .builderFor(restClientAdapter)
+                .build();
+        return httpServiceProxyFactory.createClient(ProductServiceClient.class);
+    }
+}
---
+++ order-service/src/main/java/com/pranay/ecommerce/order_service/clients/UserServiceClient.java
@@ -0,0 +1,13 @@
+package com.pranay.ecommerce.order_service.clients;
+
+import com.pranay.ecommerce.order_service.dtos.UserResponse;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+
+@HttpExchange
+public interface UserServiceClient {
+
+    @GetExchange("/api/users/{id}")
+    UserResponse getUserDetails(@PathVariable String id);
+}
